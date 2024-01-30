118a90b4c1b1b2508e9974f2b9b52a8e376d8e60
BIThooven <yassine.parkourist@gmail.com>
2024-01-30 03:30:07 +0100

    binary trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_right - a function that inserts a node at the right
+ * of a binary tree
+ * @value: value to be inserted
+ * @parent: the root node
+ * Return: returns the new allocated node to be inserted at the right
+*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
