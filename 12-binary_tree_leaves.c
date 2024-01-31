dc37630260173b76d633153d19fe249689cb9914
BIThooven <yassine.parkourist@gmail.com>
2024-01-31 23:13:36 +0100

    binary trees

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,20 +6,10 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	int count;
-
 	if (tree == NULL)
 		return (0);
-
-	count = 0;
-	if (tree != NULL)
-	{
-		binary_tree_leaves(tree->left);
-		if ((tree->left == NULL) && (tree->right == NULL))
-		{
-			count++;
-		}
-		binary_tree_leaves(tree->right);
-	}
-	return (count);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
